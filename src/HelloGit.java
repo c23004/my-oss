2fd6588a5543cb253292ac5da852e9c7c2f4d06e
suntech\c23004 <c23004@suntech.jp>
2026-06-29 15:34:21 +0900

    加算機能の追加

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,5 +1,12 @@
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git !");
+
+        //加算機能の追加
+        int a=10;
+        int b=5;
+        int c=a+b;
+
+        System.out.println("和: "+ c);
     }
 }
